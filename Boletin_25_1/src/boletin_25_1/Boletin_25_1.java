df26c5ceabfa0f1574ea9935b9b7197cb54fedfc
gpietrafesavieitez <gpietrafesavieitez@danielcastelao.org>
2019-04-02 14:11:21 +0200

    init

---
+++ Boletin_25_1/src/boletin_25_1/Boletin_25_1.java
@@ -0,0 +1,12 @@
+package boletin_25_1;
+
+public class Boletin_25_1{
+
+    public static void main(String[] args){
+        Interfaz1 interfaz1 = new Interfaz1();
+        interfaz1.show();
+        
+        Interfaz2 interfaz2 = new Interfaz2();
+        interfaz2.show();
+    }
+}
